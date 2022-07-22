66e53f9e3588ec35c164a30c4f0156b0df164ae6
asfaw <asfaw.aylkibet@gmail.com>
2022-07-22 15:50:48 +0300

    initial

---
+++ STIXTestProj/Program.cs
@@ -0,0 +1,156 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace STIXTestProj
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arguments = new string[] { "dinneraroundyou.com", "fnotifyd", "krystynajasinska86@gmail.com", "google.com" };
+
+            var results = IsSafeMultiple(arguments);
+            PrintResult(results);
+        }
+
+        public static void PrintResult(List<Result> results)
+        {
+            Console.WriteLine("........................................................................................");
+            foreach (Result result in results)
+            {
+                Console.WriteLine("Augument...-> " + result.Argument);
+                Console.WriteLine("IsMalicious...-> " + result.IsMalicious);
+                Console.WriteLine("Source...-> " + result.Source);
+                Console.WriteLine("........................................................................................");
+            }
+        }
+
+        public static List<Result> IsSafeMultiple(string[] args)
+        {
+            List<Result> results = new List<Result>();
+            foreach (string str in args)
+            {
+                results.Add(IsSafe(str));
+            }
+            return results;
+        }
+
+        public static Result IsSafe(string url)
+        {
+            List<Result> results = new List<Result>();
+            var allUrls = GetAllUrlsFromPattern();
+            var allEmails = GetAllEmailsFromPattern();
+            var allProcessNames = GetAllProcessNamesFromPattern();
+            //return !allUrls.Exists(x => x == url) && !allEmails.Exists(x => x == url) && !allProcessNames.Exists(x => x == url);
+
+            if (allUrls.Exists(x => x == url))
+            {
+                return new Result() { Source = "URL", IsMalicious = true, Argument = url };
+            }
+            else if (allEmails.Exists(x => x == url))
+            {
+                return new Result() { Source = "EMAIL", IsMalicious = true, Argument = url };
+
+            }
+            else if (allProcessNames.Exists(x => x == url))
+            {
+                return new Result() { Source = "PROCESS", IsMalicious = true, Argument = url };
+
+            }
+            else
+            {
+                return new Result() { Source = "-", IsMalicious = false, Argument = url };
+
+            }
+        }
+
+        public static Root LoadStixFile()
+        {
+            Root res = JsonConvert.DeserializeObject<Root>(File.ReadAllText("stix.json"));
+            return res;
+        }
+
+        public static List<string> GetAllStixPatterns()
+        {
+            Root stix = LoadStixFile();
+            List<string> patterns = new List<string>();
+            foreach (Object obj in stix.objects)
+            {
+                if (obj.pattern_type != null && obj.pattern_type.Equals("stix") && obj.pattern != null)
+                {
+                    patterns.Add(obj.pattern);
+                }
+            }
+            return patterns;
+        }
+
+        public static List<string> GetAllUrlsFromPattern()
+        {
+            List<string> allPatterns = GetAllStixPatterns();
+            List<string> allUrls = new List<string>();
+            foreach (string str in allPatterns)
+            {
+                if (str.Contains("domain-name"))
+                {
+                    string url = ExtractUrlFromPattern(str);
+                    allUrls.Add(url);
+                }
+
+            }
+            return allUrls;
+        }
+        public static List<string> GetAllEmailsFromPattern()
+        {
+            List<string> allPatterns = GetAllStixPatterns();
+            List<string> allUrls = new List<string>();
+            foreach (string str in allPatterns)
+            {
+                if (str.Contains("email-addr"))
+                {
+                    string url = ExtractEmailFromPattern(str);
+                    allUrls.Add(url);
+                }
+
+            }
+            return allUrls;
+
+        }
+        public static List<string> GetAllProcessNamesFromPattern()
+        {
+            List<string> allPatterns = GetAllStixPatterns();
+            List<string> allUrls = new List<string>();
+            foreach (string str in allPatterns)
+            {
+                if (str.Contains("process:name"))
+                {
+                    string url = ExtractProcessNameFromPattern(str);
+                    allUrls.Add(url);
+                }
+
+            }
+            return allUrls;
+        }
+
+        public static string ExtractUrlFromPattern(string pattern)
+        {
+            return ExtractString(pattern, 20);
+        }
+
+        public static string ExtractEmailFromPattern(string pattern)
+        {
+            return ExtractString(pattern, 19);
+        }
+        public static string ExtractProcessNameFromPattern(string pattern)
+        {
+            return ExtractString(pattern, 15);
+        }
+        public static string ExtractString(string pattern, int start)
+        {
+            string url = pattern.Substring(start, pattern.Length - 2 - start);
+            return url;
+        }
+
+    }
+}
---
+++ STIXTestProj/models.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STIXTestProj
+{
+    public class Object
+    {
+        public string type { get; set; }
+        public string spec_version { get; set; }
+        public string id { get; set; }
+        public DateTime created { get; set; }
+        public DateTime modified { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public bool is_family { get; set; }
+        public List<string> indicator_types { get; set; }
+        public string pattern { get; set; }
+        public string pattern_type { get; set; }
+        public string pattern_version { get; set; }
+        public DateTime? valid_from { get; set; }
+        public string relationship_type { get; set; }
+        public string source_ref { get; set; }
+        public string target_ref { get; set; }
+    }
+
+    public class Root
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public List<Object> objects { get; set; }
+    }
+
+    public class Result
+    {
+        public string Source { get; set; }
+        public bool IsMalicious { get; set; }
+        public string Argument { get; set; }
+    }
+}
